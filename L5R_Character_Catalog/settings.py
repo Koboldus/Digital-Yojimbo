6274d0073912acca7c93fc569da763a167bcfdaa
Koboldus <filip.jarmulak@gmail.com>
2023-03-13 15:11:15 +0100

    Beginning of account management

--- L5R_Character_Catalog/settings.py
+++ L5R_Character_Catalog/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'L5R_Character_Catalog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': '127.0.0.1',
+        'NAME': 'postgres',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
     }
 }
 
