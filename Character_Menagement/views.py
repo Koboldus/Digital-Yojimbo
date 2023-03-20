b87666f37162c3c3a7af3fe26e607470395c75c3
Koboldus <filip.jarmulak@gmail.com>
2023-03-20 13:18:50 +0100

    Changed character sheet

--- Character_Menagement/views.py
+++ Character_Menagement/views.py
@@ -3,11 +3,12 @@ from django.views import View
 from django.http import HttpResponse
 from Character_Menagement.models import Character
 
+
 # The character sheet view
 class CharacterSheet(View):
     def get(self, request, id):
         character = Character.objects.get(id=id)
         if request.user.id != character.user_id:
-            return HttpResponse("You can't look at other people's characters")
+            return HttpResponse("This isn't one of your characters")
 
         return render(request, 'character_sheet.html', {'character': character})
