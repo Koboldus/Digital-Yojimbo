5622f7e69c563e62c37ce1a90f425434b02a510f
Koboldus <filip.jarmulak@gmail.com>
2023-04-07 13:05:29 +0200

    Finished css styling for now

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-from secret import key
+from config.secret import key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
